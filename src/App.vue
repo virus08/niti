34543bd6e34208f26bbb0fda860d8f167b6a21fb
Wanchai Fuangmali <Wanchai@vstecs.co.th>
2019-11-23 12:21:46 +0700

    save-this

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,117 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+      <v-list dense>
+        <template v-for="item in items">
+          <v-row v-if="item.heading" :key="item.heading" align="center">
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+            </v-col>
+            <v-col cols="6" class="text-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-col>
+          </v-row>
+          <v-list-group
+            v-else-if="item.children"
+            :key="item.text"
+            v-model="item.model"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon
+          >
+            <template v-slot:activator>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title @click.stop="menuclick(item.text)">{{ item.text }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+            <v-list-item v-for="(child, i) in item.children" :key="i" link>
+              <v-list-item-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title @click.stop="menuclick(item.text)">{{ child.text }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item v-else :key="item.text" link>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title @click.stop="menuclick(item.text)">{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">{{site.name}}</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn icon large>
+        <v-avatar size="32px" item>
+          <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify" />
+        </v-avatar>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-layout child-flex>
+          <router-view></router-view>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  props: {
+    source: String
+  },
+  data: () => ({
+    site: {},
+    dialog: false,
+    drawer: false,
+    items: [
+      { icon: "mdi-contacts", text: "Contacts" },
+      { icon: "mdi-history", text: "Frequently contacted" },
+      { icon: "mdi-content-copy", text: "Duplicates" },
+      {
+        icon: "mdi-chevron-up",
+        "icon-alt": "mdi-chevron-down",
+        text: "Labels",
+        model: true,
+        children: [{ icon: "mdi-plus", text: "Create label" }]
+      },
+      {
+        icon: "mdi-chevron-up",
+        "icon-alt": "mdi-chevron-down",
+        text: "More",
+        model: false,
+        children: [
+          { text: "Import" },
+          { text: "Export" },
+          { text: "Print" },
+          { text: "Undo changes" },
+          { text: "Other contacts" }
+        ]
+      },
+      { icon: "mdi-settings", text: "Settings" },
+      { icon: "mdi-message", text: "Send feedback" },
+      { icon: "mdi-help-circle", text: "Help" },
+      { icon: "mdi-cellphone-link", text: "App downloads" },
+      { icon: "mdi-keyboard", text: "Go to the old version" }
+    ]
+  }),
+  methods: {
+    menuclick(txt) {
+      alert(txt);
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/services/auth.service.js
@@ -0,0 +1,58 @@
+import * as Msal from 'msal';
+
+export default class AuthService {
+  constructor() {
+    let redirectUri = window.location.origin;
+    this.applicationConfig = {
+      clientID: 'd8ae0c56-3d9a-4e5b-b333-b082701e853e',
+      graphScopes: ['user.read']
+    };
+    this.app = new Msal.UserAgentApplication(
+      this.applicationConfig.clientID,
+      '',
+      () => {
+        // callback for login redirect
+      },
+      {
+        redirectUri
+      }
+    );
+  }
+  login() {
+    return this.app.loginPopup(this.applicationConfig.graphScopes).then(
+      idToken => {
+        const user = this.app.getUser();
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+      () => {
+        return null;
+      }
+    );
+  };
+  logout() {
+    this.app.logout();
+  };
+  getToken() {
+    return this.app.acquireTokenSilent(this.applicationConfig.graphScopes).then(
+      accessToken => {
+        return accessToken;
+      },
+      error => {
+        return this.app
+          .acquireTokenPopup(this.applicationConfig.graphScopes)
+          .then(
+            accessToken => {
+              return accessToken;
+            },
+            err => {
+              console.error(err);
+            }
+          );
+      }
+    );
+  };
+}
